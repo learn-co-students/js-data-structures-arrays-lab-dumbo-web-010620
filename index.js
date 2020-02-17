dc7cf7b5918f87a8704e6a2882bb66dd878dcf78
github username <github email address>
2020-02-16 20:53:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+let drivers = ["Milo", "Otis", "Garfield"];
+
+
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+    drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift();
+}
+
+function appendDriver(name) {
+let arrayTwo = [...drivers, "Broom"];
+return arrayTwo;
+}
+
+function prependDriver(name) {
+    let arrayTwo = ["Arnold", ...drivers];
+    return arrayTwo;
+}
+
+function removeLastDriver() {
+    let newDrivers = drivers.slice(0, drivers.length -1 );
+    return newDrivers;
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1);
+}
